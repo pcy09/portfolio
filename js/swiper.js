22e8a4e1f89056ed56daf497d2016a3d46a9f1b0
pcy09 <pcy09@naver.com>
2022-12-22 02:16:58 +0900

    푸터수정

--- js/swiper.js
+++ js/swiper.js
@@ -1,9 +1,8 @@
 // Initialize Swiper
-
 var swiper = new Swiper(".mySwiper", {
-	slidesPerView: 3,
-	spaceBetween: 30,
-	slidesPerGroup: 3,
+	slidesPerView: 1,
+	spaceBetween: 0,
+	slidesPerGroup: 1,
 	loop: true,
 	loopFillGroupWithBlank: true,
 
@@ -14,5 +13,16 @@ var swiper = new Swiper(".mySwiper", {
 	navigation: {
 		nextEl: ".swiper-button-next",
 		prevEl: ".swiper-button-prev",
+	},
+	breakpoints: {
+		850: {
+			slidesPerView: 2,
+			spaceBetween: 30,
+			slidesPerGroup: 2,
+		},
+		1250: {
+			slidesPerView: 3,
+			slidesPerGroup: 3,
+		},
 	},
 });
